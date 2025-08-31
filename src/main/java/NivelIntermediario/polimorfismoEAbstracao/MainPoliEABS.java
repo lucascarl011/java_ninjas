b83e32a8913b5cf31f8ec8e85233169b18a3affa
lucascarl011 <lucassscarlosss54@gmail.com>
2025-08-31 00:28:14 -0300

    Add put

--- src/main/java/NivelIntermediario/polimorfismoEAbstracao/MainPoliEABS.java
+++ src/main/java/NivelIntermediario/polimorfismoEAbstracao/MainPoliEABS.java
@@ -5,11 +5,15 @@ public class MainPoliEABS {
 
         Uzumaki naruto = new Uzumaki();
         naruto.nome = "Naruto Uzumaki";
+        naruto.aldeia = "Aldeia da Folha";
+        naruto.idade = 16;
         naruto.habilidadeEspecial();
         naruto.estrategiaDeBatalha();
 
         Uchiha sasuke = new Uchiha();
         sasuke.nome = "Sasuke Uchiha";
+        sasuke.aldeia = "Aldeia da Folha";
+        sasuke.idade = 19;
         sasuke.habilidadeEspecial();
         sasuke.estrategiaDeBatalha();
     }
