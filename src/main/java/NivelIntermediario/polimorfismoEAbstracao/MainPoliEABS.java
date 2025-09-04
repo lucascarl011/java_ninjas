bdafc273fbfdc856007a1d85d6ff5abaabf513bd
lucascarl011 <lucassscarlosss54@gmail.com>
2025-09-03 22:54:08 -0300

    Atualiacoes na Main

--- src/main/java/NivelIntermediario/polimorfismoEAbstracao/MainPoliEABS.java
+++ src/main/java/NivelIntermediario/polimorfismoEAbstracao/MainPoliEABS.java
@@ -6,6 +6,7 @@ public class MainPoliEABS {
         System.out.println("------------- Ninja Naruto -------------");
         Uzumaki naruto = new Uzumaki("Naruto Uzumaki", " Aldeia da Folha", 17, 15, NivelNinja.JOUNIN);
         System.out.println(naruto);
+        naruto.estrategiaDeBatalha();
         naruto.habilidadeEspecial();
         naruto.inteligenciaDeCombate(150);
         naruto.inteligenciaDeCombate();
@@ -14,6 +15,7 @@ public class MainPoliEABS {
         System.out.println("------------- Ninja Sasuke -------------");
         Uchiha sasuke = new Uchiha("Sasuke Uchiha", "Aldeia da Folha", 19, 20, NivelNinja.GENIN);
         System.out.println(sasuke);
+        sasuke.estrategiaDeBatalha();
         sasuke.habilidadeEspecial();
         sasuke.inteligenciaDeCombate(300);
         sasuke.inteligenciaDeCombate();
