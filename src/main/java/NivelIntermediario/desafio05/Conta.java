fc8d3a80f75318b769a2836a8538b8dd96f8bf16
lucascarl011 <lucassscarlosss54@gmail.com>
2025-09-13 18:28:29 -0300

    Adicionando saque e tranferencia na interface Conta

--- src/main/java/NivelIntermediario/desafio05/Conta.java
+++ src/main/java/NivelIntermediario/desafio05/Conta.java
@@ -3,6 +3,7 @@ package NivelIntermediario.desafio05;
 public interface Conta {
 
     double consultarSaldo();
-
     void depositar(double valorDeposito);
+    void sacar(double valorSaque);
+    void transferir(double valorTransferencia, Conta destino);
 }
