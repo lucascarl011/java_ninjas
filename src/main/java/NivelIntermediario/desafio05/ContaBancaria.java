cdd565dd6e12436b666b78a96fb1ff3b01a44105
lucascarl011 <lucassscarlosss54@gmail.com>
2025-09-13 18:38:26 -0300

    Implementacao do metodo transferir na Classe ContaBancaria

--- src/main/java/NivelIntermediario/desafio05/ContaBancaria.java
+++ src/main/java/NivelIntermediario/desafio05/ContaBancaria.java
@@ -18,16 +18,23 @@ public abstract class ContaBancaria implements Conta{
     public void sacar(double valorSaque){
          if (valorSaque > 0 && valorSaque <= saldo) {
              saldo -= valorSaque;
-             System.out.println("Saque realizado com sucesso: -" + valorSaque);
+             System.out.println("Saque realizado com sucesso: R$ -" + valorSaque);
          }else {
              System.out.println("Saldo insuficiente");
          }
     }
 
+    @Override
+    public void transferir(double valorTransferencia, Conta destino){
+         if (valorTransferencia > 0 && valorTransferencia <= saldo) {
+             this.sacar(valorTransferencia);
+             destino.depositar(valorTransferencia);
+             System.out.println("Transferência realizada com sucesso: R$ -" + valorTransferencia);
+         } else  {
+             System.out.println("Saldo insuficiente para tranferência!");
+         }
+    }
+
      @Override
      public abstract void depositar(double valorDeposito);
-
-
-
-
 }
