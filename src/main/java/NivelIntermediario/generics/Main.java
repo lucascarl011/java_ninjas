2cefd20fbe4e77bd00c8a644203eece6c30c8c2b
CONTABIL01 <CONTABIL01@kgt.com.local>
2025-11-11 17:46:35 -0300

    Finalizando Classe Main

--- src/main/java/NivelIntermediario/generics/Main.java
+++ src/main/java/NivelIntermediario/generics/Main.java
@@ -12,5 +12,13 @@ package NivelIntermediario.generics;
 public class Main {
     public static void main(String[] args) {
 
+        EquipamentosNinja adagaDeFogo = new EquipamentosNinja("Adaga De Fogo");
+        EquipamentosNinja adagaDeAgua = new EquipamentosNinja("Adaga de Agua");
+
+        BolsaGenerica<EquipamentosNinja> bolsaGenerica = new BolsaGenerica<>();
+        bolsaGenerica.adicionarEquipamentos(adagaDeAgua);
+        bolsaGenerica.adicionarEquipamentos(adagaDeFogo);
+
+        System.out.println(bolsaGenerica);
     }
 }
