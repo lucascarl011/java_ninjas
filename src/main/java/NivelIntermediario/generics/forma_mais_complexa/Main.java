12dabd04e1a8adb7f1b228dc0acfbeca3f47c02d
lucascarl011 <lucassscarlosss54@gmail.com>
2025-11-13 22:31:11 -0300

    Generics - Adicionando Classe Main e testando as funcionalidades

--- src/main/java/NivelIntermediario/generics/forma_mais_complexa/Main.java
+++ src/main/java/NivelIntermediario/generics/forma_mais_complexa/Main.java
@@ -9,5 +9,15 @@ public class Main {
          * 2 - Facilitar manutenção
          * 3 - Código reutilizável e genérico
          * */
+
+        // PAssamos como parametro da Lista a SuperClasse "Object" pois ela nos permite trabalhar com diversos tipos de variaveis
+        BolsaNinja<Object> bolsaNinja = new BolsaNinja<>();
+        bolsaNinja.adicionarEquipamento(new Kunai("Kunai Explosiva"));
+        bolsaNinja.adicionarEquipamento(new Kunai("Kunai de Fogo"));
+        bolsaNinja.adicionarEquipamento(new Pergaminho("Frases para Eternidade"));
+        bolsaNinja.adicionarEquipamento(new Shuriken(3));
+
+        System.out.println("Itens da Bolsa Ninja: ");
+        bolsaNinja.mostrarEquipamentos();
     }
 }
