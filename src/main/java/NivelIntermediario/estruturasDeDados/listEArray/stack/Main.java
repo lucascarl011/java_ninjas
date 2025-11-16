37257cd62bfb6e097c69f1a3b20b92f090902bc0
lucascarl011 <lucassscarlosss54@gmail.com>
2025-11-16 17:18:52 -0300

    Stack - Finalizacao do estudo de pilha com Stack

--- src/main/java/NivelIntermediario/estruturasDeDados/listEArray/stack/Main.java
+++ src/main/java/NivelIntermediario/estruturasDeDados/listEArray/stack/Main.java
@@ -19,6 +19,24 @@ public class Main {
 
         // Stack
         // O ultimo elemento é obrigatoriamente o primeiro a sair
-        Stack<String> ninjasStack = new Stack<>();
+        Stack<String> ninjasStack = new Stack<>(); // Criação da minha pilha com o tipo "String"
+
+        // Para adicionar elementos na minha pilha = "push"
+        ninjasStack.push("Naruto Uzumaki");
+        ninjasStack.push("Sasuke Uchiha");
+        ninjasStack.push("Sakura Haruno");
+        ninjasStack.push("Kakashi Hatake");
+        ninjasStack.push("Hinata Hyuga");
+        System.out.println("Minha Stack Atual = " + ninjasStack);
+
+        // Para Remover o último elemento da minha pilha = "pop"
+        ninjasStack.pop();
+        System.out.println("Minha Stack Atualizada com pop = " + ninjasStack);
+
+        // Para visualizar o próximo elemento a sair da minha pilha = "peek"
+        System.out.println("Minha Stack com o proximo elemento do topo com peek = " + ninjasStack.peek());
+
+        // Para visualizar o tamanho da minha pilha = "size"
+        System.out.println("Tamanho da minha Stack com size = " + ninjasStack.size() + " elementos");
     }
 }
