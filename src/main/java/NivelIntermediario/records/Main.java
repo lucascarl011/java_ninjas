dfaca73fe7eee34dae726244f48288bc3386046f
lucascarl011 <lucassscarlosss54@gmail.com>
2025-11-16 16:36:19 -0300

    Records - finalizacao da classe Main

--- src/main/java/NivelIntermediario/records/Main.java
+++ src/main/java/NivelIntermediario/records/Main.java
@@ -3,8 +3,19 @@ package NivelIntermediario.records;
 public class Main {
     public static void main(String[] args) {
 
+        // Sem records
         Ninja cadastro = new Ninja("Naruto", "naruto@email.com", 99999999);
-
+        System.out.println("----- Sem Records ------");
         System.out.println("Dados do cadastro: " + cadastro);
+        System.out.println("Dados do cadastro: " + cadastro.getNome());
+
+        System.out.println();
+
+        // Com records
+        NinjaRecords cadastroRecord = new NinjaRecords("Sasuke", "sasuke@email.com", 77777777);
+        System.out.println("----- Com Records ------");
+        System.out.println("Dados do cadastro com Records: " + cadastroRecord);
+        System.out.println("Dados do cadastro com Records: " + cadastroRecord.nome());
+        System.out.println("Dados do cadastro com Records: " + cadastroRecord.emailCaixaAlta());
     }
 }
--- src/main/java/NivelIntermediario/records/NinjaRecords.java
+++ src/main/java/NivelIntermediario/records/NinjaRecords.java
@@ -1,4 +1,12 @@
 package NivelIntermediario.records;
 
-public record NinjaRecords() {
+// Apenas isso tem a mesma função da Claase Ninja, com as mesma funcionalidade
+public record NinjaRecords(String nome, String email, int telefone) {
+
+    // Todos os atributos são FINAL por padrão
+    // Records não tem SETTER - Não tem como alterar o atributo
+
+    public String emailCaixaAlta() {
+        return email.toUpperCase();
+    }
 }
